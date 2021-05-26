ee802a0b3a52f23814ef74c80477da4e485e8f2b
HaDocK <arnaucosta23@gmail.com>
2021-05-26 13:53:01 +0200

    test prod 11

---
+++ dist/precache-manifest.dc9ec6e929b1979aa5be656c4d1cbb1f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6c281c02c845e104b4",
+    "url": "/css/app.143f9335.css"
+  },
+  {
+    "revision": "8342a271ac7093f375fe",
+    "url": "/css/chunk-02734058.dca4f9f6.css"
+  },
+  {
+    "revision": "b3aa75fe25663a62c67e",
+    "url": "/css/chunk-04c3359c.2f3ed124.css"
+  },
+  {
+    "revision": "66967b1dd7b48f1ab029",
+    "url": "/css/chunk-6df33844.24f07a86.css"
+  },
+  {
+    "revision": "6996cdca53bfce373e13",
+    "url": "/css/chunk-cc060e34.1eeb37e3.css"
+  },
+  {
+    "revision": "95296856df7e099db734",
+    "url": "/css/chunk-f824f630.bccc76b9.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon2.ico"
+  },
+  {
+    "revision": "1f9a3545c334ae9f22b2b9aa2a45ec3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae6c281c02c845e104b4",
+    "url": "/js/app.66e12342.js"
+  },
+  {
+    "revision": "8342a271ac7093f375fe",
+    "url": "/js/chunk-02734058.55ec2d09.js"
+  },
+  {
+    "revision": "b3aa75fe25663a62c67e",
+    "url": "/js/chunk-04c3359c.06be4486.js"
+  },
+  {
+    "revision": "66967b1dd7b48f1ab029",
+    "url": "/js/chunk-6df33844.cd0ed506.js"
+  },
+  {
+    "revision": "6996cdca53bfce373e13",
+    "url": "/js/chunk-cc060e34.b017c986.js"
+  },
+  {
+    "revision": "95296856df7e099db734",
+    "url": "/js/chunk-f824f630.8c2fb164.js"
+  },
+  {
+    "revision": "8bde1bdbbc29a5d4d087",
+    "url": "/js/chunk-vendors.365c0bd2.js"
+  },
+  {
+    "revision": "2539397506a01191e1cb89bb02c3915f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
