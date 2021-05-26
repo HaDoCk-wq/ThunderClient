0ad6f069ce3851513c21dec8644cd8875fc36b84
HaDocK <arnaucosta23@gmail.com>
2021-05-26 11:19:30 +0200

    test7 prod

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   { path: '/', redirect: '/channels/' },
@@ -41,7 +41,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
